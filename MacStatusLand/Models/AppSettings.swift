e45d97e513dfc158bf2ecb3fc7016fa12a64efae
xiaoRui278 <742397974@qq.com>
2026-05-29 18:05:11 +0800

    cleanup: remove unused Accessibility services and views

--- MacStatusLand/Models/AppSettings.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct AppSettings: Codable {
-    var launchAtLogin: Bool = true
-    var autoHideOnLaunch: Bool = true
-    var hiddenAppIdentifiers: Set<String> = []
-
-    static let `default` = AppSettings()
-}
