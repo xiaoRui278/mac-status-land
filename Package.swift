7d3944effd72a216cce8289502a48d18e2abaef9
xiaoRui278 <742397974@qq.com>
2026-05-29 17:26:56 +0800

    fix: exclude Info.plist and Entitlements from SPM target

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
     targets: [
         .executableTarget(
             name: "MacStatusLand",
-            path: "MacStatusLand"
+            path: "MacStatusLand",
+            exclude: ["Info.plist", "Entitlements.entitlements"]
         )
     ]
 )
