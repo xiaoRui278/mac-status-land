f2226375c778b847bc7e63c8ef85d9dadf3de68b
xiaoRui278 <742397974@qq.com>
2026-06-01 09:36:17 +0800

    fix: use Bundle.module for SPM resource loading

--- MacStatusLand/Services/StringLocalization.swift
+++ MacStatusLand/Services/StringLocalization.swift
@@ -4,11 +4,11 @@ import SwiftUI
 extension String {
     func localized(_ language: String? = nil) -> String {
         let lang = language ?? SettingsService.shared.appLanguage
-        guard let path = Bundle.main.path(forResource: lang, ofType: "lproj"),
+        guard let path = Bundle.module.path(forResource: lang, ofType: "lproj"),
               let bundle = Bundle(path: path) else {
-            return NSLocalizedString(self, comment: "")
+            return NSLocalizedString(self, tableName: nil, bundle: Bundle.module, comment: "")
         }
-        return NSLocalizedString(self, tableName: nil, bundle: bundle, value: "", comment: "")
+        return NSLocalizedString(self, tableName: nil, bundle: bundle, comment: "")
     }
     
     func localizedFormat(_ language: String? = nil, _ args: CVarArg...) -> String {
